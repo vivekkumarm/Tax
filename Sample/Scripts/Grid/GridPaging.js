b6335adc3bd29862a40f8f98340d002b345c15a5
vivekkumarm <mvivekkumar89@karunya.edu.in>
2013-10-21 12:09:52 +0530

    Tax

---
+++ Sample/Scripts/Grid/GridPaging.js
@@ -0,0 +1,39 @@
+﻿var initialProduct = [
+           { product: "Tikki Burger", sales: 400, price: 25.50 },
+           { product: "Ham Burger", sales: 60, price: 30.00 },
+           { product: "Cheese Burger", sales: 85, price: 25.00 },
+           { product: "Double Cheese Burger", sales: 48, price: 40.00 },
+           { product: "Fries", sales: 90, price: 30.00 },
+           { product: "Cold Drink(Small)", sales: 450, price: 25.00 },
+           { product: "Cold Drink(Medium)", sales: 420, price: 35.00 }
+];
+
+function GridView(items) {
+    this.items = ko.observableArray(items);
+
+    this.addProduct = function () {
+        this.items.push({ product: "New Product", sales: 0, price: 30.00 });
+    };
+
+    this.sortProduct = function () {
+        this.items.sort(function (a, b) {
+            return a.product < b.product ? -1 : 1;
+        });
+    };
+
+    this.goToStartingPage = function () {
+        this.gridViewModel.currentPageIndex(0);
+    };
+
+    this.gridViewModel = new ko.simpleGrid.viewModel({
+        data: this.items,
+        columns: [
+            { headerText: "Items", rowText: "product" },
+            { headerText: "Sales", rowText: "sales" },
+            { headerText: "Price", rowText: function (item) { return "$" + item.price.toFixed(2) } }
+        ],
+        pageSize: 4
+    });
+};
+
+ko.applyBindings(new GridView(initialProduct));
---
+++ Sample/Scripts/Popup/Popupdialog.js
@@ -0,0 +1,33 @@
+﻿var jQueryWidget = function (element, valueAccessor, name, constructor) {
+    var options = ko.utils.unwrapObservable(valueAccessor());
+    var $element = $(element);
+    setTimeout(function () { constructor($element, options) }, 0);
+    //$element.data(name, $widget);
+
+};
+
+ko.bindingHandlers.dialog = {
+    init: function (element, valueAccessor, allBindingsAccessor, viewModel) {
+        console.log("init");
+        jQueryWidget(element, valueAccessor, 'dialog', function ($element, options) {
+            console.log("Creating dialog on " + $element);
+            return $element.dialog(options);
+        });
+    }
+};
+
+
+ko.bindingHandlers.dialogcmd = {
+    init: function (element, valueAccessor, allBindingsAccessor, viewModel) {
+        $(element).button().click(function () {
+            var options = ko.utils.unwrapObservable(valueAccessor());
+            $('#' + options.id).dialog(options.cmd || 'open');
+        });
+    }
+};
+
+var viewModel = {
+    label: ko.observable('dialog test')
+};
+
+ko.applyBindings(viewModel);
